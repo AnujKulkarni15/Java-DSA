762d01594344cc4ece29c84b93f2c5b0b0502654
Anuj Kulkarni <116297311+AnujKulkarni15@users.noreply.github.com>
2024-06-27 10:19:19 +0530

    Create linearSearch.java

---
+++ linearSearch.java
@@ -0,0 +1,21 @@
+public class linearSearch {
+  public static void main(String[] args) {
+    int nums[] = { 8, 7, 9, 5, 4, 6 };
+    int tar = 5;
+    int ans = search(nums, tar);
+    System.out.println(ans);
+  }
+
+  public static int search(int arr[], int target) {
+    if (arr.length == 0) {
+      return -1;
+    }
+    for (int i = 0; i < arr.length; i++) {
+      int element = arr[i];
+      if (element == target) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
