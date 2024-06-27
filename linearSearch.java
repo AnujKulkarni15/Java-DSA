44bca7ada1265e6e49998f573163af996bd1d85b
Anuj Kulkarni <116297311+AnujKulkarni15@users.noreply.github.com>
2024-06-27 10:27:55 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -21,3 +21,28 @@ public class linearSearch {
     return -1;
   }
 }
+
+
+
+// For searching a character in a string 
+
+public class linearSearch {
+  public static void main(String[] args) {
+    String name = "anuj";
+    char target = 'u';
+    System.out.println(search(name, target));
+  }
+
+  public static boolean search(String str, char target) {
+    if (str.length() == 0) {
+      return false;
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      if (target == str.charAt(i)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
